b85a10926c30ecfca6536d68a5675ee13aae2bb0
martin <martin@V-DOXIE-W10>
2016-07-06 17:51:13 +0800

    Add project files.

---
+++ 7.6KeyBoard/Page1.xaml.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _7._6KeyBoard
+{ 
+    public sealed partial class Page1 : Page
+    {
+
+        uint numActiveContacts;
+        Windows.Devices.Input.TouchCapabilities touchCapabilities = new Windows.Devices.Input.TouchCapabilities();
+
+        Dictionary<uint, Windows.UI.Xaml.Input.Pointer> contacts;
+        public Page1()
+        {
+            this.InitializeComponent();
+
+            numActiveContacts = 0;
+            // Initialize the dictionary.
+            contacts = new Dictionary<uint, Windows.UI.Xaml.Input.Pointer>((int)touchCapabilities.Contacts);
+            // Declare the pointer event handlers.
+            Target.PointerPressed += new PointerEventHandler(Target_PointerPressed);
+            Target.PointerEntered += new PointerEventHandler(Target_PointerEntered);
+            Target.PointerReleased += new PointerEventHandler(Target_PointerReleased);
+            Target.PointerExited += new PointerEventHandler(Target_PointerExited);
+            Target.PointerCanceled += new PointerEventHandler(Target_PointerCanceled);
+            Target.PointerCaptureLost += new PointerEventHandler(Target_PointerCaptureLost);
+            Target.PointerMoved += new PointerEventHandler(Target_PointerMoved);
+            Target.PointerWheelChanged += new PointerEventHandler(Target_PointerWheelChanged);
+
+            buttonClear.Click += new RoutedEventHandler(ButtonClear_Click);
+
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            eventLog.Text = "";
+        }
+
+
+        void Target_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nDown: " + ptr.PointerId;
+
+            // Change background color of target when pointer contact detected.
+            Target.Fill = new SolidColorBrush(Windows.UI.Colors.Green);
+
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+
+            // Lock the pointer to the target.
+            Target.CapturePointer(e.Pointer);
+
+            // Update event sequence.
+            eventLog.Text += "\nPointer captured: " + ptr.PointerId;
+
+            // Check if pointer already exists (for example, enter occurred prior to press).
+            if (contacts.ContainsKey(ptr.PointerId))
+            {
+                return;
+            }
+            // Add contact to dictionary.
+            contacts[ptr.PointerId] = ptr;
+            ++numActiveContacts;
+
+            // Display pointer details.
+            createInfoPop(e);
+        }
+
+        private void Target_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nEntered: " + ptr.PointerId;
+
+            if (contacts.Count == 0)
+            {
+                // Change background color of target when pointer contact detected.
+                Target.Fill = new SolidColorBrush(Windows.UI.Colors.Blue);
+            }
+
+            // Check if pointer already exists (if enter occurred prior to down).
+            if (contacts.ContainsKey(ptr.PointerId))
+            {
+                return;
+            }
+
+            // Add contact to dictionary.
+            contacts[ptr.PointerId] = ptr;
+            ++numActiveContacts;
+
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+
+            // Display pointer details.
+            createInfoPop(e);
+        }
+
+        private void Target_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;        
+            if (ptr.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                Windows.UI.Input.PointerPoint ptrPt = e.GetCurrentPoint(Target);
+                if (ptrPt.Properties.IsLeftButtonPressed)
+                {
+                    eventLog.Text += "\nLeft button: " + ptrPt.PointerId;
+                }
+                if (ptrPt.Properties.IsMiddleButtonPressed)
+                {
+                    eventLog.Text += "\nWheel button: " + ptrPt.PointerId;
+                }
+                if (ptrPt.Properties.IsRightButtonPressed)
+                {
+                    eventLog.Text += "\nRight button: " + ptrPt.PointerId;
+                }
+            }
+
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+
+            // Display pointer details.
+            updateInfoPop(e);
+        }
+
+
+        private void Target_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nMouse wheel: " + ptr.PointerId;
+
+            // Check if pointer already exists (for example, enter occurred prior to wheel).
+            if (contacts.ContainsKey(ptr.PointerId))
+            {
+                return;
+            }
+
+            // Add contact to dictionary.
+            contacts[ptr.PointerId] = ptr;
+            ++numActiveContacts;
+
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+
+            // Display pointer details.
+            createInfoPop(e);
+        }
+
+
+        void Target_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nUp: " + ptr.PointerId;
+            if (ptr.PointerDeviceType != Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                // Update target UI.
+                Target.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+
+                destroyInfoPop(ptr);
+
+                // Remove contact from dictionary.
+                if (contacts.ContainsKey(ptr.PointerId))
+                {
+                    contacts[ptr.PointerId] = null;
+                    contacts.Remove(ptr.PointerId);
+                    --numActiveContacts;
+                }
+
+                // Release the pointer from the target.
+                Target.ReleasePointerCapture(e.Pointer);
+
+                // Update event sequence.
+                eventLog.Text += "\nPointer released: " + ptr.PointerId;
+
+                // Prevent most handlers along the event route from handling the same event again.
+                e.Handled = true;
+            }
+            else
+            {
+                Target.Fill = new SolidColorBrush(Windows.UI.Colors.Blue);
+            }
+        }
+
+
+        private void Target_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nPointer exited: " + ptr.PointerId;
+
+            // Remove contact from dictionary.
+            if (contacts.ContainsKey(ptr.PointerId))
+            {
+                contacts[ptr.PointerId] = null;
+                contacts.Remove(ptr.PointerId);
+                --numActiveContacts;
+            }
+
+            // Update the UI and pointer details.
+            destroyInfoPop(ptr);
+
+            if (contacts.Count == 0)
+            {
+                Target.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+        }
+
+
+        private void Target_PointerCanceled(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nPointer canceled: " + ptr.PointerId;
+
+            // Remove contact from dictionary.
+            if (contacts.ContainsKey(ptr.PointerId))
+            {
+                contacts[ptr.PointerId] = null;
+                contacts.Remove(ptr.PointerId);
+                --numActiveContacts;
+            }
+
+            destroyInfoPop(ptr);
+
+            if (contacts.Count == 0)
+            {
+                Target.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+        }
+
+
+        //捕获丢失
+        private void Target_PointerCaptureLost(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Xaml.Input.Pointer ptr = e.Pointer;
+
+            // Update event sequence.
+            eventLog.Text += "\nPointer capture lost: " + ptr.PointerId;
+
+            // Remove contact from dictionary.
+            if (contacts.ContainsKey(ptr.PointerId))
+            {
+                contacts[ptr.PointerId] = null;
+                contacts.Remove(ptr.PointerId);
+                --numActiveContacts;
+            }
+
+            destroyInfoPop(ptr);
+
+            if (contacts.Count == 0)
+            {
+                Target.Fill = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+            // Prevent most handlers along the event route from handling the same event again.
+            e.Handled = true;
+        }
+
+
+        void createInfoPop(PointerRoutedEventArgs e)
+        {
+            TextBlock pointerDetails = new TextBlock();
+            Windows.UI.Input.PointerPoint ptrPt = e.GetCurrentPoint(Target);
+            pointerDetails.Name = ptrPt.PointerId.ToString();
+            pointerDetails.Foreground = new SolidColorBrush(Windows.UI.Colors.White);
+            pointerDetails.Text = queryPointer(ptrPt);
+
+            TranslateTransform x = new TranslateTransform();
+            x.X = ptrPt.Position.X + 20;
+            x.Y = ptrPt.Position.Y + 20;
+            pointerDetails.RenderTransform = x;
+
+            Container.Children.Add(pointerDetails);
+        }
+
+
+        void updateInfoPop(PointerRoutedEventArgs e)
+        {
+            foreach (var pointerDetails in Container.Children)
+            {
+                if (pointerDetails.GetType().ToString() == "Windows.UI.Xaml.Controls.TextBlock")
+                {
+                    TextBlock _TextBlock = (TextBlock)pointerDetails;
+                    if (_TextBlock.Name == e.Pointer.PointerId.ToString())
+                    {
+                        // To get pointer location details, we need extended pointer info.
+                        // We get the pointer info through the getCurrentPoint method
+                        // of the event argument. 
+                        Windows.UI.Input.PointerPoint ptrPt = e.GetCurrentPoint(Target);
+                        TranslateTransform x = new TranslateTransform();
+                        x.X = ptrPt.Position.X + 20;
+                        x.Y = ptrPt.Position.Y + 20;
+                        pointerDetails.RenderTransform = x;
+                        _TextBlock.Text = queryPointer(ptrPt);
+                    }
+                }
+            }
+        }
+
+        String queryPointer(PointerPoint ptrPt)
+        {
+            String details = "";
+
+            switch (ptrPt.PointerDevice.PointerDeviceType)
+            {
+                case Windows.Devices.Input.PointerDeviceType.Mouse:
+                    details += "\nPointer type: mouse";
+                    break;
+                case Windows.Devices.Input.PointerDeviceType.Pen:
+                    details += "\nPointer type: pen";
+                    if (ptrPt.IsInContact)
+                    {
+                        details += "\nPressure: " + ptrPt.Properties.Pressure;
+                        details += "\nrotation: " + ptrPt.Properties.Orientation;
+                        details += "\nTilt X: " + ptrPt.Properties.XTilt;
+                        details += "\nTilt Y: " + ptrPt.Properties.YTilt;
+                        details += "\nBarrel button pressed: " + ptrPt.Properties.IsBarrelButtonPressed;
+                    }
+                    break;
+                case Windows.Devices.Input.PointerDeviceType.Touch:
+                    details += "\nPointer type: touch";
+                    details += "\nrotation: " + ptrPt.Properties.Orientation;
+                    details += "\nTilt X: " + ptrPt.Properties.XTilt;
+                    details += "\nTilt Y: " + ptrPt.Properties.YTilt;
+                    break;
+                default:
+                    details += "\nPointer type: n/a";
+                    break;
+            }
+
+            GeneralTransform gt = Target.TransformToVisual(this);
+            Point screenPoint;
+
+            screenPoint = gt.TransformPoint(new Point(ptrPt.Position.X, ptrPt.Position.Y));
+            details += "\nPointer Id: " + ptrPt.PointerId.ToString() +
+                "\nPointer location (parent): " + ptrPt.Position.X + ", " + ptrPt.Position.Y +
+                "\nPointer location (screen): " + screenPoint.X + ", " + screenPoint.Y;
+            return details;
+        }
+
+        void destroyInfoPop(Windows.UI.Xaml.Input.Pointer ptr)
+        {
+            foreach (var pointerDetails in Container.Children)
+            {
+                if (pointerDetails.GetType().ToString() == "Windows.UI.Xaml.Controls.TextBlock")
+                {
+                    TextBlock _TextBlock = (TextBlock)pointerDetails;
+                    if (_TextBlock.Name == ptr.PointerId.ToString())
+                    {
+                        Container.Children.Remove(pointerDetails);
+                    }
+                }
+            }
+        }
+    }
+}
